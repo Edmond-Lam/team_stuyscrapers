ce89580260cada1fab7081826c7cf0ac68de1abf
Issac Kim <ikim1@stuy.edu>
2017-03-30 14:42:49 -0400

    make box appear when hovering

--- static/hover.js
+++ static/hover.js
@@ -2,8 +2,20 @@ var svg = document.getElementById("slate");
 
 var showInfo = function(){
     var x = this.getAttribute("cx");
-    var y = this.getAttribute("cy");
-    alert(x + y);
+    var y = parseInt(this.getAttribute("cy")) - 30;
+    var box = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+    box.setAttribute("width", 50);
+    box.setAttribute("height", 30);
+    box.setAttribute("fill", "black");
+    box.setAttribute("id", "text");
+    box.setAttribute("x", x);
+    box.setAttribute("y", y);
+    svg.appendChild(box);
+};
+
+var removeInfo = function(){
+    var text = document.getElementById("text");
+    text.parentNode.removeChild(text);
 }
 
 var drawCircle = function(e){
@@ -18,6 +30,7 @@ var drawCircle = function(e){
     dot.setAttribute("r", r);
     dot.setAttribute("class", "dot");
     dot.addEventListener("mouseover", showInfo);
+    dot.addEventListener("mouseout", removeInfo);
     svg.appendChild(dot);
 };
 
