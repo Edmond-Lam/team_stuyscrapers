42ecc80db5705fca6c8be2c01ccfb21af73d0577
Edmond-Lam <elam3@stuy.edu>
2017-04-01 15:21:54 -0400

    Added gitignore so you won't have to worry about those pesky extra files emacs leave behind. Slider stuff still in progress.

--- slider.js
+++ slider.js
@@ -1,18 +1,22 @@
 var $element = $('input[type="range"]');
 var $handle;
+var $text = $( "#text" )
 
 $element
-  .rangeslider({
-    polyfill: false,
-    onInit: function() {
-      $handle = $('.rangeslider__handle', this.$range);
-      updateHandle($handle[0], this.value);
-    }
-  })
-  .on('input', function() {
-    updateHandle($handle[0], this.value);
-  });
+    .rangeslider({
+	polyfill: false,
+	onInit: function() {
+	    $handle = $('.rangeslider__handle', this.$range);
+	    updateInfo($handle[0], this.value);
+	    updateInfo($text, this.value);
+	}
+    })
+    .on('input', function() {
+	updateInfo($handle[0], this.value);
+	text.textContent = this.value;
+    });
 
-function updateHandle(el, val) {
-  el.textContent = val;
+function updateInfo(el, val) {
+    el.textContent = val;
 }
+
