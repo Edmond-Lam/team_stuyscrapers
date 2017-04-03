846ccf487058522b23a53e3d4008a39fb01fc6dd
Maddie Ostergaard <mostergaard@stuy.edu>
2017-04-03 14:46:12 -0400

    parser.py

--- utils/parser.py
+++ utils/parser.py
@@ -1,4 +1,4 @@
-import csv
++import csv
 
 def open():
     with open('skyscraper.csv') as csvfile:
@@ -6,12 +6,20 @@ def open():
     return reader
 
 
-def getInfo():
+def getInfo(year):
     reader = open()
+    dict = {}
     for row in reader:
-        
+        if row[8] <= year && row[8] > 0:
+            l = {'city':row[5], 'height':row[16], "latitude":row[23], "longitude":row[25], "name":row[29]}
+            if not row[8] in dict.keys:
+                dict[row[8]] = [];
+            dict[row[8]].append(l)
 
 
+
+
+            
 """
 def getHeight():
     reader = open()
