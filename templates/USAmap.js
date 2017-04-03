1331f9fe25ed3ad056cddfca4cc22b64ef7ee23e
Costas100 <constantineathanitis@yahoo.com>
2017-04-03 16:13:38 -0400

    finished dropdown menu

--- templates/USAmap.js
+++ templates/USAmap.js
@@ -15,3 +15,28 @@ d3.json("https://unpkg.com/us-atlas@1/us/10m.json", function(error, us) {
       .attr("d", path(topojson.feature(us, us.objects.nation)));
 });
 
+var viewButton = document.getElementById("view");
+//var viewButton = d3.select("select");
+//console.log(viewButton);
+
+
+
+var changeColor = function(e){
+    //the console log codes are pseudo codes for the function
+    //we need to display the various views
+
+    
+    if (viewButton.value == "height"){
+	console.log("height"); 
+    }
+    
+    else if (viewButton.value == "materials"){
+	console.log("materials");
+    }
+
+    else if (viewButton.value == "usage"){
+	console.log("usage");
+    }
+}
+console.log("height");//by default show the height view
+viewButton.addEventListener("change", changeColor);
