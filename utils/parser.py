b15ce739dd2067ec6e8bc6719dea037938fea01f
Issac Kim <ikim1@stuy.edu>
2017-04-03 16:35:58 -0400

    parse csv into json

--- utils/parser.py
+++ utils/parser.py
@@ -1,23 +1,40 @@
-+import csv
-
-def open():
-    with open('skyscraper.csv') as csvfile:
-    reader = csv.DictReader(csvfile)
-    return reader
+import csv
+import json
 
 
-def getInfo(year):
-    reader = open()
-    dict = {}
-    for row in reader:
-        if row[8] <= year && row[8] > 0:
-            l = {'city':row[5], 'height':row[16], "latitude":row[23], "longitude":row[25], "name":row[29]}
-            if not row[8] in dict.keys:
-                dict[row[8]] = [];
-            dict[row[8]].append(l)
+def getInfo(file):
+    dic = {}
+    with open(file, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            year = row['completed.year']
+            if year != 0:
+                year = str(year)
+                if year not in dic.keys():
+                    dic[year] = []
+                building = {}
+                building['name'] = row['name']
+                building['height'] = row['height']
+                building['material'] = row['material']
+                building['location'] = [row['latitude'], row['longitude']]
+                building['city'] = row['city']
+                purpose = []
+                if row['telecommunications']:
+                    purpose.append('telecommunications')
+                if row['residential']:
+                    purpose.append('residential')
+                if row['office']:
+                    purpose.append('office')
+                building['purpose'] = purpose
+                dic[year].append(building)
+    return dic
 
+def makeJson(fileIn, fileOut):
+    with open(fileOut, 'w') as fo:
+        data = getInfo(fileIn)
+        json.dump(data, fo)
 
-
+makeJson('../data/skyscrapers.csv', '../data/skyscrapers.json')
 
             
 """
