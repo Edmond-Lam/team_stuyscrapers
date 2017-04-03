87d6675a3f2e7edf6fa5d8da1927aeda1adf7a2d
Costas100 <constantineathanitis@yahoo.com>
2017-04-03 16:53:28 -0400

    started testing points

--- templates/USAmap.js
+++ templates/USAmap.js
@@ -2,6 +2,7 @@ var svg = d3.select("svg");
 
 var path = d3.geoPath();
 
+//loads the USA map data
 d3.json("https://unpkg.com/us-atlas@1/us/10m.json", function(error, us) {
   if (error) throw error;
 
@@ -9,10 +10,37 @@ d3.json("https://unpkg.com/us-atlas@1/us/10m.json", function(error, us) {
 
   svg.append("path")
       .attr("stroke-width", 0.5)
-      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
+	.attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
 
-  svg.append("path")
-      .attr("d", path(topojson.feature(us, us.objects.nation)));
+    svg.append("path")
+	.attr("d", path(topojson.feature(us, us.objects.nation)));
+
+
+    var projection = d3.geo.albersUsa();
+    
+    svg.selectAll(".pin")
+	.data(places)
+	.enter().append("circle", ".pin")
+	.attr("r", "50px")
+
+    console.log(projection([d.location.longitude]));
+     /**   .attr("cx",function(d) {
+      return "" + projection([
+          d.location.longitude])});
+
+	/**.attr("cx","500")//projection[d.location.longitude])
+        .attr("cy","500");//projection[d.location.latitude]);
+
+     add circles to svg
+    svg.selectAll("circle")
+	.data([aa,bb]).enter()
+	.append("circle")
+	.attr("cx", function (d) { console.log(projection(d)); return projection(d)[0]; })
+	.attr("cy", function (d) { return projection(d)[1]; })
+	.attr("r", "100px")
+	.attr("fill", "red")
+**/
+    
 });
 
 var viewButton = document.getElementById("view");
@@ -40,3 +68,23 @@ var changeColor = function(e){
 }
 console.log("height");//by default show the height view
 viewButton.addEventListener("change", changeColor);
+
+
+//trying to plot points
+
+var places = [
+  {
+    name: "New York City",
+    location: {
+      latitude: 40.7128,
+      longitude: -74.0059
+    }
+  },
+  {
+    name: "Houston",
+    location: {
+      latitude: 29.7604,
+      longitude: -95.3698
+    }
+  }
+]
