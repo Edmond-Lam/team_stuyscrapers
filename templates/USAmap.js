a11c62c0f135e6def7262c4cfd0c899698eed302
Costas100 <constantineathanitis@yahoo.com>
2017-04-04 16:54:57 -0400

    finally added points to map

--- templates/USAmap.js
+++ templates/USAmap.js
@@ -3,7 +3,9 @@ var svg = d3.select("svg");
 var path = d3.geoPath();
 
 //loads the USA map data
-d3.json("https://unpkg.com/us-atlas@1/us/10m.json", function(error, us) {
+
+var us = "https://unpkg.com/us-atlas@1/us/10m.json";
+d3.json(us, function(error, us) {
   if (error) throw error;
 
 
@@ -15,33 +17,68 @@ d3.json("https://unpkg.com/us-atlas@1/us/10m.json", function(error, us) {
     svg.append("path")
 	.attr("d", path(topojson.feature(us, us.objects.nation)));
 
+////////////////////////////////////////////////////////////////////////
+    //THIS CREATES A RANDOM CIRCLE...aka it works
+    /**
 
-    var projection = d3.geo.albersUsa();
+    svg.append("circle")
+//	.data(places)
+//	.enter()
+
+	.attr("cy",200)
+	.attr("cx", 200)
+
+//	.attr("cy",function(d){return d[0]["location"]["latitude"];}) //latitude
+//	.attr("cx",function(d){return d[0]["location"]["longitude"];}) //longitude
     
-    svg.selectAll(".pin")
-	.data(places)
-	.enter().append("circle", ".pin")
 	.attr("r", "50px")
+	.style("fill", "red");
 
-    console.log(projection([d.location.longitude]));
-     /**   .attr("cx",function(d) {
-      return "" + projection([
-          d.location.longitude])});
+    ////////////////////////////////////////////////////////////////////
 
-	/**.attr("cx","500")//projection[d.location.longitude])
-        .attr("cy","500");//projection[d.location.latitude]);
-
-     add circles to svg
-    svg.selectAll("circle")
-	.data([aa,bb]).enter()
+    d3.select("svg").selectAll("circle")
+    	.data(longi)
+    	.enter()
 	.append("circle")
-	.attr("cx", function (d) { console.log(projection(d)); return projection(d)[0]; })
-	.attr("cy", function (d) { return projection(d)[1]; })
-	.attr("r", "100px")
-	.attr("fill", "red")
-**/
+
+    //longitude
+    	.attr("cx",function(d){return d;})
     
+    
+	.attr("r", "50px")
+	.style("fill", "red");**/
+
+    d3.select("svg").selectAll("circle")
+	.data(places)
+	.enter()
+	.append("circle")
+    	.attr("cy",function(d){
+	    return d["location"]["latitude"];
+	}) //latitude
+    	.attr("cx",function(d){
+	    return d["location"]["longitude"];
+	}) //longitude
+	.attr("r", "50px")
+	.style("fill", "red");
+    
+
+    //idk why the cy isn't being added
+    d3.select("svg").selectAll("circle")
+    	.data(lat)
+    	.enter()
+	.append("circle")    
+    	.attr("cy",function(d){return d;}) //latitude
+ 
+   // console.log(places[0]["location"]["latitude"]);
+    //console.log(places[0]["location"]["longitude"]);
 });
+
+
+
+var longi = [74,95]
+var lat = [40,29]
+
+
 
 var viewButton = document.getElementById("view");
 //var viewButton = d3.select("select");
@@ -49,7 +86,7 @@ var viewButton = document.getElementById("view");
 
 
 
-var changeColor = function(e){
+var changeView = function(e){
     //the console log codes are pseudo codes for the function
     //we need to display the various views
 
@@ -65,9 +102,10 @@ var changeColor = function(e){
     else if (viewButton.value == "usage"){
 	console.log("usage");
     }
-}
-console.log("height");//by default show the height view
-viewButton.addEventListener("change", changeColor);
+};
+
+//console.log("height");//by default show the height view
+viewButton.addEventListener("change", changeView);
 
 
 //trying to plot points
@@ -76,8 +114,8 @@ var places = [
   {
     name: "New York City",
     location: {
-      latitude: 40.7128,
-      longitude: -74.0059
+      latitude: 40.7128,//y
+      longitude: -74.0059//x
     }
   },
   {
@@ -88,3 +126,75 @@ var places = [
     }
   }
 ]
+
+console.log("latitude: "+places[0]["location"]["latitude"]);
+console.log("longitude: "+places[0]["location"]["longitude"]);
+
+//PARSE TROUGH THE DICTIONARYYYYY
+
+
+
+
+     //var projection = d3.geo.albersUsa();
+    /**
+    svg.selectAll("circle")
+	.data(places)
+	.enter().append("circle", ".pin")
+	.attr("r", "50px")
+
+   // console.log(projection([d.location.longitude]));
+    /**   .attr("cx",function(d) {
+	  return "" + projection([
+          d.location.longitude])});
+
+	  /**.attr("cx","500")//projection[d.location.longitude])
+          .attr("cy","500");//projection[d.location.latitude]);
+
+	  add circles to svg
+
+    svg.selectAll("circle")
+	.data(places).enter()
+	.append("circle")
+	.attr("cx", "200");
+	.attr("cy", "200");
+	.attr("r", "100px")
+	.attr("fill", "red")
+    
+  
+
+
+
+    circles.selectAll("div")
+	.data(lat)
+	.enter()
+	.append("circle")
+	.attr("cy", function(d){return d+"px";}) //latitude
+	.attr("r",30);
+
+    
+    circles.selectAll("div")
+	.data(longi)
+	.enter()
+	.append("circle")
+	.attr("cx", function(d){return d+"px";}) //longitude
+	.attr("r",30);
+    
+	});
+
+
+    svg.selectAll("circle")
+	.data([aa,bb]).enter()
+	.append("circle")
+	.attr("cx",100)
+	      //function (d) { console.log(projection(d)); return projection(d)[0]; })
+	.attr("cy",100)
+	      //function (d) { return projection(d)[1]; })
+	.attr("r", "50px")
+	.attr("fill", "red")
+
+    aa = [-122.490402, 37.786453];
+    bb = [-122.389809, 37.72728];
+
+    
+});
+**/
